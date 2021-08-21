d135809ccc494c0c0cd1aca3d35de3f6e4ca29ff
anggidast <adastariana@gmail.com>
2021-08-21 21:16:22 +0700

    fix: validationn error message

--- models/validation.go
+++ models/validation.go
@@ -33,17 +33,17 @@ func (cv *CustomValidator) Validate(i interface{}) error {
 			return echo.NewHTTPError(http.StatusBadRequest, map[string]string{
 				"message": "Password cannot be empty/null",
 			})
-		case "Key: 'Todo.Title' Error:Field validation for 'Title' failed on the 'required' tag Key: 'Todo.Due_date' Error:Field validation for 'Due_date' failed on the 'required' tag":
+		case "Key: 'Todo.Title' Error:Field validation for 'Title' failed on the 'required' tag\nKey: 'Todo.Due_date' Error:Field validation for 'Due_date' failed on the 'required' tag":
 			return echo.NewHTTPError(http.StatusBadRequest, map[string]string{
-				"message": "Todo title and due date cannot be empty/null",
+				"message": "Title and due date cannot be empty/null",
 			})
 		case "Key: 'Todo.Due_date' Error:Field validation for 'Due_date' failed on the 'required' tag":
 			return echo.NewHTTPError(http.StatusBadRequest, map[string]string{
-				"message": "Todo title and due date cannot be empty/null",
+				"message": "Due date cannot be empty/null",
 			})
 		case "Key: 'Todo.Title' Error:Field validation for 'Title' failed on the 'required' tag":
 			return echo.NewHTTPError(http.StatusBadRequest, map[string]string{
-				"message": "Todo title and due date cannot be empty/null",
+				"message": "Title cannot be empty/null",
 			})
 		}
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
